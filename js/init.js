48455c19e290e3396e354ce2e5db20a4b8f3a54b
superadonis15 <74470525+superadonis15@users.noreply.github.com>
2020-11-14 15:21:07 -0500

    Comprobatorio_Mauricio_mendoza

---
+++ js/init.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMcontentloaded' ,() => {
+
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Slider.init(elems);
+
+  var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems);
+
+});
